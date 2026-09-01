53f2157f7ac182cd64f4c90e7a4c4319fc05474b
Yi Xun <myixun007@gmail.com>
2026-09-01 18:42:07 +0800

    Add custom behaviour to DialogBox

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -90,8 +90,8 @@ public class Main extends Application {
         String userText = userInput.getText();
         String dukeText = duke.getResponse(userInput.getText());
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, userImage),
-                new DialogBox(dukeText, dukeImage));
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage));
         userInput.clear();
     }
 }

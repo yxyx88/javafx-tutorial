b2f24291e407c69c8aaba3eda3db148275d84f70
Yi Xun <myixun007@gmail.com>
2026-09-01 18:31:42 +0800

    Update GUI to echo the user

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,9 +30,6 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
@@ -41,7 +38,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
-        //More code to be added here later
+        //Formatting the window to look as expected
 
         stage.setTitle("Duke");
         stage.setResizable(false);
@@ -71,6 +68,23 @@ public class Main extends Application {
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
+        //Handling user input
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+    }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        userInput.clear();
     }
 }
 
